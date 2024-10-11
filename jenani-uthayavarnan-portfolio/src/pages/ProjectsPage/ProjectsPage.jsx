7b2062ab674783f2a95bd912ce55c4899b5b8f3e
Jenani Uthayavarnan <jenaniu@hotmail.com>
2024-10-11 16:27:36 -0400

    add git hub link

--- jenani-uthayavarnan-portfolio/src/pages/ProjectsPage/ProjectsPage.jsx
+++ jenani-uthayavarnan-portfolio/src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,18 +1,20 @@
 import './ProjectsPage.scss'
+import { Link } from 'react-router-dom'
+
 
 function ProjectPage() {
 
-  return (
-    <>
-      <h1>Projects</h1>
-      <h2>That's Our Lingo</h2>
-      <p>A full-stack gamified language learning app that enables the user to memorize key vocabulary terms while <br></br>
-      gaining a deeper understanding of their target language.
-
-
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>Projects</h1>
+            <Link to='https://github.com/jenaniu/jenani-uthayavarnan-thats-our-lingo'>
+                <h2>That's Our Lingo</h2>
+            </Link>
+            <p>A full-stack gamified language learning app that enables the user to memorize key vocabulary terms while <br></br>
+                gaining a deeper understanding of their target language.
+            </p>
+        </>
+    )
 }
 
 export default ProjectPage
